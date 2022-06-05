577c12ab13b15d5142d6db56c623942058f6e8ce
6loodrain <89647718+6loodrain@users.noreply.github.com>
2022-06-05 11:29:18 +0500

    Update README.me and port_scanner.py

--- README.md
+++ README.md
@@ -20,12 +20,13 @@ Web-приложение для сканирования открытых TCP п
 ## Запуск программы
 Загрузите репозиторий и перейдите в папку. В командной строке выполните следующую инструкцию.
 
-        python port_scanner.py
-Откройте браузер и сделайте запрос на указанный ниже адрес.
-    
-        https:://localhost:8080/scan/127.0.0.1/1/1000
+`python port_scanner.py`
 
-Таким образом вы просканируете удалённый хост по адресу 127.0.0.1 в диапазоне портов от 1 до 1000 включительно.
+Откройте браузер и введите адрес указанный ниже.
+    
+`https:://localhost:8080/scan/127.0.0.1/1/1000`
+
+Таким образом вы просканируете удалённый хост по адресу `127.0.0.1` в диапазоне портов от `1` до `1000` включительно.
 ## Пример запроса и вывода на примере сайта github.com
 ### 1.  Запускаем программу
 ![Image alt](https://sun9-east.userapi.com/sun9-23/s/v1/ig2/hZ8Uix6tbp2hWEjqVoVKeOm3RUf1ggtWoW_TlNguSwqah7Qs77vHpJ7kw45QxGSssNB8LaAPeTV5AEzavtkxK0JX.jpg?size=533x71&quality=96&type=album)
@@ -34,17 +35,17 @@ Web-приложение для сканирования открытых TCP п
 ### 2.  Вводим необходимые параметры в консоль
 ![Image Alt](https://sun9-north.userapi.com/sun9-81/s/v1/ig2/zRjjY9iOJewoWm15Bv7P4TF8d7AD3ItdDVmUINt2pGY06BoyC6asJekUcBBEHcoXtePAQm4_PxYr2C7-PKEaRkH1.jpg?size=557x102&quality=96&type=album)
 
-#### !!!Допустимый формат IP - от 0.0.0.0 до 255.255.255.255!!!
-#### !!!Допустимый формат start_port и end_port 0 < start_port <= end_port!!!
+#### Допустимый формат IP - от `0.0.0.0` до `255.255.255.255`
+#### Допустимый формат start_port и end_port `0` < start_port <= end_port
 ### 3.  Получаем результат
 ![Image Alt](https://sun9-west.userapi.com/sun9-48/s/v1/ig2/qslPIDdoAeVniHME31tFSXtkliipDIJhFi8Kzc8fO3tYTMj8axF1MfupFVRE04Oce7KN0DamPi5a4AQoZLD-A3vj.jpg?size=1920x1033&quality=96&type=album)
 ## Структура проекта
 1. port_scanner.py - исполняемый файл
 2. README.md - у вас перед глазами
-3. specifications.txt - версии используемых библиотек и python
+3. requirements.txt - файл для установки всех зависимостей с помощью `pip freeze`
 4. test_port_scanner.py - набор тестов
 
 ## Важно
-Если вы работаете с большим диапазоном портов, от 5000, рекомендуется увеличить время жизни соединения. По умолчанию стоит 5 секунд.
+Если вы работаете с большим диапазоном портов, от `5000`, рекомендуется увеличить время жизни соединения. По умолчанию стоит 5 секунд.
 
-        awaited_time = 5  # in sec, duration of connection
+        awaited_time = 5  # in sec
--- port_scanner.py
+++ port_scanner.py
@@ -4,25 +4,18 @@ import logging
 import json
 
 
-# create connection with ip, port, event loop, awaited_time
 async def check_connection(ip, port, scan_loop, awaited_time):
     conn = asyncio.open_connection(ip, port, loop=scan_loop)
     try:
-        # wait for response for awaited_time
         await asyncio.wait_for(conn, awaited_time)
-        # receive response
         return {"port": str(port), "state": "open"}
-    except asyncio.TimeoutError:  # use awaited_time to start async timer
-        # not receive response in awaited_time
+    except asyncio.TimeoutError:
         return {"port": str(port), "state": "closed"}
 
 
-# iterator
 async def iterate(ip, begin_port, end_port, scan_loop, awaited_time):
-    # create task with ip, port range, awaited_time
     task = [check_connection(ip, port, scan_loop, awaited_time) for port in
             range(begin_port, end_port + 1)]
-    # await fulfilling
     response = await asyncio.gather(*task)
     return response
 
@@ -30,71 +23,61 @@ async def iterate(ip, begin_port, end_port, scan_loop, awaited_time):
 # validation
 def check_input(request):
     try:
-        # split and check ip
+
         ip = request.match_info.get('ip').split('.')
+
         if type(ip) != 'list' and len(ip) != 4:
-            raise ValueError('IP is incorrect.')
+            logging.error('IP is incorrect.')
+            raise Exception()
         for item in ip:
             if not 0 <= int(item) <= 255:
-                raise Exception(f'({item}) in IP is incorrect.')
-        # check begin_port and end_port
+                logging.error(f'({item}) in IP is incorrect.')
+                raise Exception()
+
         begin_port = int(request.match_info.get('begin_port'))
         end_port = int(request.match_info.get('end_port'))
+
         if not 0 < begin_port <= 65535:
-            raise ValueError(f'({begin_port}) - begin_port out of range.')
+            logging.error(f'({begin_port}) - begin_port out of range.')
+            raise Exception()
         if not 0 < end_port <= 65535:
-            raise ValueError(f'({end_port}) - end_port out of range.')
+            logging.error(f'({end_port}) - end_port out of range.')
+            raise Exception()
         if not begin_port <= end_port:
-            raise ValueError(f'begin_port ({begin_port}) is bigger than end_port ({end_port}).')
-    except Exception as e:
-        print(e)
-        # with error
+            logging.error(f'begin_port ({begin_port}) is bigger than end_port ({end_port}).')
+            raise Exception()
+
+    except Exception:
         return False
-    # without any errors
     return True
 
 
-# handler
 async def handle(request):
-    if check_input(request):  # validation is OK
+    if check_input(request):
+
         ip = request.match_info.get('ip')
         begin_port = int(request.match_info.get('begin_port'))
         end_port = int(request.match_info.get('end_port'))
-        # param = request.match_info.get('param')
-        # print(request)
-        # print(type(request))
-        awaited_time = 5  # in sec, duration of connection
-        scan_loop = asyncio.get_running_loop()  # IDK, but in some wrote it should speed up program
+
+        awaited_time = 5  # in sec
+        scan_loop = asyncio.get_running_loop()
         response = await iterate(ip, begin_port, end_port, scan_loop, awaited_time)
-        # print(results)
-        # buffer = []
-        # if param == 'open':
-        #    [buffer.append(item) for item in results if item['state'] == 'open']
-        #    response = buffer
-        # elif param == 'closed':
-        #    [buffer.append(item) for item in results if item['state'] == 'closed']
-        #    response = buffer
 
-        return web.Response(text=json.dumps(response), status=200)  # format to json
-    else:  # request is NOT OK
-        return web.Response(text='400: Bad Request', status=400)  # format to json
+        return web.Response(text=json.dumps(response), status=200)
+    else:
+        return web.Response(text='400: Bad Request', status=400)
 
 
-# initializing app
 def create_app():
     app = web.Application()
-    # choose specific listening route, start handler
     app.router.add_get('/scan/{ip}/{begin_port}/{end_port}', handle)
-    # app.router.add_get('/scan/{ip}/{begin_port}/{end_port}/{param}', handle)
     return app
 
 
-# logger
 logger = logging.getLogger('aiohttp.access')
-logger.setLevel(logging.DEBUG)  # aiohttp.access time GMT+0000 request + HTTP1.1 IDK Browser version
-# logger.addHandler(logging.StreamHandler()) # log in console
-logging.basicConfig(filename='syslog.log', encoding='utf-8', level=logging.DEBUG)  # log in file
+logger.setLevel(logging.DEBUG)
+# default log in console
+# logging.basicConfig(filename='syslog.log', encoding='utf-8', level=logging.DEBUG)  # log in file
 
-# start program
 if __name__ == '__main__':
     web.run_app(create_app())
